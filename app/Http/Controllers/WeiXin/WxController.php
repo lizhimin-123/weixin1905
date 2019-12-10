1e093932a8ab21ac9ee73647728aa4c7e39c3fb8
lizhimin <2763855398@qq.com>
2019-12-10 11:27:41 +0800

    微信接入

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -8,7 +8,27 @@ use Illuminate\Http\Request;
 class WxController extends Controller
 {
     //
-    public function wechat(){
+    public function index()
+    {
+        $token = 'token';       //开发提前设置好的 token````````````````
+        $signature = $_GET["signature"];
+        $timestamp = $_GET["timestamp"];
+        $nonce = $_GET["nonce"];
+        $echostr = $_GET["echostr"];
 
+
+
+        $tmpArr = array($token, $timestamp, $nonce);
+        sort($tmpArr, SORT_STRING);
+        $tmpStr = implode( $tmpArr );
+        $tmpStr = sha1( $tmpStr );
+
+
+
+        if( $tmpStr == $signature ){        //验证通过
+            echo $echostr;
+        }else{
+            die("not ok");
+        }
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 Route::get('/test/','Test\TestController@admin');
 
 //微信开发
-Route::get('/Wx/','WeiXin\WxController@wechat');
+Route::get('/Wx/','WeiXin\WxController@index');
 
 
 
