004227670866b034e2f809f4a5e5c5475e72bec4
lizhimin <2763855398@qq.com>
2019-12-11 11:53:18 +0800

    php://input

--- app/Http/Controllers/WeiXin/WxController.php
+++ app/Http/Controllers/WeiXin/WxController.php
@@ -40,8 +40,9 @@ class WxController extends Controller
      */
      public function receiv(){
          $log_file = "wx.log";
+         $xml = file_get_contents("php://input");
          //将接收的数据记录到日志文件
-         $data = data('Y-m-d H:i:s') . json_encode($_POST);
+         $data = data('Y-m-d H:i:s') . $xml;
          file_put_contents($log_file,$data,FILE_APPEND);
      }
     /***
