10088b4e92dd21c0ca13180bf88340ea542ccfe2
lizhimin <2763855398@qq.com>
2019-12-21 10:07:19 +0800

    gai

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -29,7 +29,7 @@ class VoteController extends Controller
     protected function getAccessToken($code){
         $url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid='.env('APPID').'&secret='.env('APPSECRET').'&code='.$code.'&grant_type=authorization_code';
         $json_data=file_get_contents($url);
-        $data=json_encode($json_data,true);
+        $data=json_decode($json_data,true);
 //        echo '<pre>';print_r($data);echo'</pre>';
         if (isset($data['errcode'])){
             //TODO 错误处理
